c68a57083ce52669f3982a6107017032ccf291ff
gipteqqo <gipteqqo@pussport.com>
2021-10-06 12:41:35 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Footer from './components/Footer';
 import NavMenu from './components/NavMenu';
 import ScrollToTop from './components/ScrollToTop';
-import About from './pages/About';
 import Contact from './pages/Contact';
 import Home from './pages/Home';
 
@@ -14,9 +13,6 @@ export default function App() {
         <NavMenu />
         <ScrollToTop />
         <Switch>
-          <Route path="/about">
-            <About />
-          </Route>
           <Route path="/contact">
             <Contact />
           </Route>
